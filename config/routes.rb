75da8e0453db52ae73531f9c117354ea54ad4908
Valerie Smith <valerie@Valeries-MacBook-Pro.local>
2019-10-23 21:47:32 -0500

    added professors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :professors
   resources :offices
   resources :courses
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_023246) do
+ActiveRecord::Schema.define(version: 2019_10_24_024551) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "number"
+    t.string "department"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "offices", force: :cascade do |t|
     t.string "room_number"
@@ -18,4 +26,13 @@ ActiveRecord::Schema.define(version: 2019_10_24_023246) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "professors", force: :cascade do |t|
+    t.string "name"
+    t.integer "office_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["office_id"], name: "index_professors_on_office_id"
+  end
+
+  add_foreign_key "professors", "offices"
 end
